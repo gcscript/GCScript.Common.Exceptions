b119a2357d22326903cb00ad7fc7986d080cd5a3
Gustavo Costa <gcs5stars@gmail.com>
2024-11-04 23:38:32 -0300

    Initial commit

---
+++ GCScriptException.cs
@@ -0,0 +1,13 @@
+﻿namespace GCScript.Common.Exceptions;
+public class GCScriptException : System.Exception {
+	public int Code { get; }
+
+	public GCScriptException(int code, string message = "An unexpected error occurred =(")
+		: base(message) {
+		Code = code;
+	}
+
+	public override string ToString() {
+		return $"GCScriptException: {Code} - {Message}\n{StackTrace}";
+	}
+}
